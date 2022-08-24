453960a6f14c76e2f932278dbe1668134183faa0
kj-thejoin <108256829+kj-thejoin@users.noreply.github.com>
2022-08-24 16:18:00 +0900

    login

--- reservation_test/lib/pages/login/login.dart
+++ reservation_test/lib/pages/login/login.dart
@@ -15,90 +15,111 @@ class Login extends StatelessWidget {
       // appBar: AppBar(
       //   title: Text('Login 페이지'),
       // ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Form(
-              child: Theme(
-                data: ThemeData(
-                  primaryColor: Colors.blue,
-                  inputDecorationTheme: InputDecorationTheme(
-                    labelStyle: TextStyle(color: Colors.blue, fontSize: 15.0),
+      body: Builder(builder: (context) {
+        return GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 100.0),
+                ),
+                Form(
+                  child: Theme(
+                    data: ThemeData(
+                      primaryColor: Colors.blue,
+                      inputDecorationTheme: InputDecorationTheme(
+                        labelStyle:
+                            TextStyle(color: Colors.blue, fontSize: 15.0),
+                      ),
+                    ),
+                    child: Container(
+                      padding: EdgeInsets.all(40.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            'TheJoin',
+                            style: TextStyle(
+                              fontSize: 40.0,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          Text(
+                            'Internal Service',
+                            style: TextStyle(
+                              fontSize: 18.0,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 30.0,
+                          ),
+                          TextField(
+                            // autofocus: true,
+                            decoration: InputDecoration(
+                              labelText: 'ID를 입력하세요',
+                              enabledBorder: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(width: 3, color: Colors.blue),
+                              ),
+                            ),
+                            keyboardType: TextInputType.text,
+                            controller: userId,
+                          ),
+                          SizedBox(
+                            height: 15.0,
+                          ),
+                          TextField(
+                            decoration: InputDecoration(
+                              labelText: '비밀번호를 입력하세요',
+                              enabledBorder: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(width: 3, color: Colors.blue),
+                              ),
+                            ),
+                            keyboardType: TextInputType.text,
+                            obscureText: true,
+                            controller: userPw,
+                          ),
+                          SizedBox(
+                            height: 15.0,
+                          ),
+                          ButtonTheme(
+                            minWidth: 100.0,
+                            height: 30.0,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                if (userId.text == "thejoin" &&
+                                    userPw.text == "1234") {
+                                  Get.toNamed(Home.id);
+                                } else {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                      content: Text('로그인 정보를 다시 확인하세요',
+                                          textAlign: TextAlign.center),
+                                      duration: Duration(seconds: 3),
+                                      backgroundColor: Colors.blue,
+                                    ),
+                                  );
+                                }
+                              },
+                              child: Text('로그인'),
+                              // style: ElevatedButton.styleFrom(),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                 ),
-                child: Container(
-                  padding: EdgeInsets.all(40.0),
-                  child: Column(
-                    children: [
-                      Text(
-                        'TheJoin',
-                        style: TextStyle(
-                          fontSize: 40.0,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      Text(
-                        'Internal Service',
-                        style: TextStyle(
-                          fontSize: 18.0,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 30.0,
-                      ),
-                      TextField(
-                        autofocus: true,
-                        decoration: InputDecoration(
-                          labelText: 'Enter your ID',
-                          enabledBorder: OutlineInputBorder(
-                            borderSide:
-                                BorderSide(width: 3, color: Colors.blue),
-                          ),
-                        ),
-                        keyboardType: TextInputType.text,
-                        controller: userId,
-                      ),
-                      SizedBox(
-                        height: 15.0,
-                      ),
-                      TextField(
-                        decoration: InputDecoration(
-                          labelText: 'Enter your password',
-                          enabledBorder: OutlineInputBorder(
-                            borderSide:
-                                BorderSide(width: 3, color: Colors.blue),
-                          ),
-                        ),
-                        keyboardType: TextInputType.text,
-                        obscureText: true,
-                        controller: userPw,
-                      ),
-                      SizedBox(
-                        height: 15.0,
-                      ),
-                      ButtonTheme(
-                        minWidth: 100.0,
-                        height: 30.0,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            if (userId.text == "thejoin" &&
-                                userPw.text == "1234") {
-                              Get.toNamed(Home.id);
-                            }
-                          },
-                          child: Text('로그인'),
-                          // style: ElevatedButton.styleFrom(),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      }),
     );
   }
 }
