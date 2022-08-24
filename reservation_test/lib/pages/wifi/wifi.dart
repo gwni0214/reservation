9da3e884b72d10c95cb28152ed3fc85ade51d258
kj-thejoin <108256829+kj-thejoin@users.noreply.github.com>
2022-08-24 17:58:40 +0900

    wifi

--- reservation_test/lib/pages/wifi/wifi.dart
+++ reservation_test/lib/pages/wifi/wifi.dart
@@ -8,7 +8,73 @@ class Wifi extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('WIFI')),
+      appBar: AppBar(
+        title: Text('WIFI 정보'),
+        backgroundColor: Color.fromARGB(
+          255,
+          26,
+          35,
+          126,
+        ),
+        centerTitle: true,
+      ),
+      body: SizedBox(
+        width: 350.0,
+        height: 550.0,
+        child: Card(
+          margin: EdgeInsets.all(30.0),
+          shape: RoundedRectangleBorder(
+            side: BorderSide(color: Colors.blue.shade900),
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 30.0, bottom: 30.0),
+              ),
+              Text(
+                '3층',
+                style: TextStyle(
+                  fontSize: 35.0,
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              SizedBox(
+                width: 200.0,
+                height: 200.0,
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(color: Colors.blue.shade900),
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  child: Text(
+                    'QR Code',
+                  ),
+                  // Image.asset('images/wifiqr.png'),
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Text(
+                'ThejoinWifi',
+                style: TextStyle(
+                  fontSize: 20.0,
+                ),
+              ),
+              Text(
+                '비밀번호 : thejoin10',
+                style: TextStyle(
+                  fontSize: 20.0,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
