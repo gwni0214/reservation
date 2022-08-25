5c5e6191d016b23744b0f3910bf45942c0c68edd
kj-thejoin <108256829+kj-thejoin@users.noreply.github.com>
2022-08-25 16:00:25 +0900

    reservWirte

--- reservation_test/lib/pages/reserve/reserve_write.dart
+++ reservation_test/lib/pages/reserve/reserve_write.dart
@@ -18,6 +18,59 @@ class ReserveWrite extends StatelessWidget {
         ),
         centerTitle: true,
       ),
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        child: Column(children: [
+          Text(
+            '3층 회의실',
+            style: TextStyle(
+              fontSize: 30.0,
+            ),
+          ),
+          Container(
+            width: 500.0,
+            height: 40.0,
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Text('09:30 ~ 10:30'),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blue.shade900,
+                textStyle: TextStyle(fontSize: 25.0),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 10.0),
+            width: 500.0,
+            height: 40.0,
+            child: Center(
+              child: Text(
+                '참여자',
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            color: Colors.blue.shade900,
+          ),
+          Container(
+            width: 500.0,
+            height: 70.0,
+            decoration: BoxDecoration(
+              // borderRadius: BorderRadius.circular(10.0),
+              color: Colors.blue.shade900,
+            ),
+            child: Text(
+              '1234',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ]),
+      ),
     );
   }
 }
