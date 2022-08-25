e8c75750301d264a95718b150cc4c7ce345aefe4
kj-thejoin <108256829+kj-thejoin@users.noreply.github.com>
2022-08-25 11:46:03 +0900

    wifi

--- reservation_test/lib/pages/wifi/wifi.dart
+++ reservation_test/lib/pages/wifi/wifi.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+// import 'package:dots_indicator/dots_indicator.dart';
+// import 'package:smooth_page_indicator/';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+// import 'AnimatedSmoothIndicator';
 
 class Wifi extends StatelessWidget {
   static const id = '/wifi';
@@ -7,6 +11,9 @@ class Wifi extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    PageController controller =
+        PageController(initialPage: 0, viewportFraction: 1);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('WIFI 정보'),
@@ -18,59 +25,136 @@ class Wifi extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: SizedBox(
-        width: 350.0,
-        height: 550.0,
-        child: Card(
-          margin: EdgeInsets.all(30.0),
-          shape: RoundedRectangleBorder(
-            side: BorderSide(color: Colors.blue.shade900),
-            borderRadius: BorderRadius.circular(20.0),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
+      body: PageView(
+        controller: controller,
+        children: [
+          Column(
             children: [
-              Padding(
-                padding: EdgeInsets.only(top: 30.0, bottom: 30.0),
-              ),
-              Text(
-                '3층',
-                style: TextStyle(
-                  fontSize: 35.0,
-                ),
-              ),
               SizedBox(
-                height: 30.0,
-              ),
-              SizedBox(
-                width: 200.0,
-                height: 200.0,
+                width: 400.0,
+                height: 450.0,
                 child: Card(
+                  margin: EdgeInsets.all(30.0),
                   shape: RoundedRectangleBorder(
                     side: BorderSide(color: Colors.blue.shade900),
                     borderRadius: BorderRadius.circular(20.0),
                   ),
-                  child: Image.asset('images/wifiqr.png'),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(top: 0.0, bottom: 10.0),
+                      ),
+                      Text(
+                        '3층',
+                        style: TextStyle(
+                          fontSize: 35.0,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      SizedBox(
+                        width: 200.0,
+                        height: 200.0,
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            side: BorderSide(color: Colors.blue.shade900),
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                          child: Image.asset('images/wifiqr.png'),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      Text(
+                        'ThejoinWifi',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                        ),
+                      ),
+                      Text(
+                        '비밀번호 : thejoin10',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-              SizedBox(
-                height: 30.0,
-              ),
-              Text(
-                'ThejoinWifi',
-                style: TextStyle(
-                  fontSize: 20.0,
-                ),
-              ),
-              Text(
-                '비밀번호 : thejoin10',
-                style: TextStyle(
-                  fontSize: 20.0,
-                ),
+              SmoothPageIndicator(
+                controller: controller, // PageController
+                count: 2,
+                effect: SlideEffect(), // your preferred effect
               ),
             ],
           ),
-        ),
+          Column(
+            children: [
+              SizedBox(
+                width: 400.0,
+                height: 450.0,
+                child: Card(
+                  margin: EdgeInsets.all(30.0),
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(color: Colors.blue.shade900),
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(top: 0.0, bottom: 10.0),
+                      ),
+                      Text(
+                        '2층',
+                        style: TextStyle(
+                          fontSize: 35.0,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      SizedBox(
+                        width: 200.0,
+                        height: 200.0,
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            side: BorderSide(color: Colors.blue.shade900),
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                          child: Image.asset('images/wifiqr.png'),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      Text(
+                        'ThejoinWifi',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                        ),
+                      ),
+                      Text(
+                        '비밀번호 : thejoin10',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SmoothPageIndicator(
+                controller: controller, // PageController
+                count: 2,
+                effect: SlideEffect(), // your preferred effect
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
