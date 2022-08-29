0ed3d4281c14c02aec79b570335e6a52e2df69db
kj-thejoin <108256829+kj-thejoin@users.noreply.github.com>
2022-08-29 15:49:51 +0900

    wifi

--- reservation_test/lib/pages/wifi/wifi.dart
+++ reservation_test/lib/pages/wifi/wifi.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
-// import 'package:get/get.dart';
+import 'package:reservation_test/pages/wifi/wifiCard.dart';
 
 class Wifi extends StatelessWidget {
   static const id = '/wifi';
-  const Wifi({Key? key}) : super(key: key);
+  const Wifi({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    PageController controller =
-        PageController(initialPage: 0, viewportFraction: 1);
-
     return Scaffold(
       appBar: AppBar(
         title: Text('WIFI 정보'),
@@ -23,133 +21,25 @@ class Wifi extends StatelessWidget {
         centerTitle: true,
       ),
       body: PageView(
-        controller: controller,
+        physics: BouncingScrollPhysics(),
         children: [
-          Column(
-            children: [
-              SizedBox(
-                width: 400.0,
-                height: 450.0,
-                child: Card(
-                  margin: EdgeInsets.all(30.0),
-                  shape: RoundedRectangleBorder(
-                    side: BorderSide(color: Colors.blue.shade900),
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.only(top: 0.0, bottom: 10.0),
-                      ),
-                      Text(
-                        '3층',
-                        style: TextStyle(
-                          fontSize: 35.0,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 20.0,
-                      ),
-                      imgbox('images/wifi3f.png'),
-                      SizedBox(
-                        height: 20.0,
-                      ),
-                      Text(
-                        'ThejoinWifi',
-                        style: TextStyle(
-                          fontSize: 20.0,
-                        ),
-                      ),
-                      Text(
-                        '비밀번호 : thejoin10',
-                        style: TextStyle(
-                          fontSize: 20.0,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              SmoothPageIndicator(
-                controller: controller, // PageController
-                count: 2,
-                effect: SlideEffect(), // your preferred effect
-              ),
-            ],
+          WifiCard(
+            floor: '3층',
+            wifiName: 'ThejoinWifi',
+            wifiPw: 'thejoin10',
+            url: 'images/wifi3f.png',
+            pagesCount: 2,
+            indicator: 0,
           ),
-          Column(
-            children: [
-              SizedBox(
-                width: 400.0,
-                height: 450.0,
-                child: Card(
-                  margin: EdgeInsets.all(30.0),
-                  shape: RoundedRectangleBorder(
-                    side: BorderSide(color: Colors.blue.shade900),
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.only(top: 0.0, bottom: 10.0),
-                      ),
-                      Text(
-                        '2층',
-                        style: TextStyle(
-                          fontSize: 35.0,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 20.0,
-                      ),
-                      imgbox('images/wifi2f.png'),
-                      SizedBox(
-                        height: 20.0,
-                      ),
-                      Text(
-                        'ThejoinLAB_WiFI',
-                        style: TextStyle(
-                          fontSize: 20.0,
-                        ),
-                      ),
-                      Text(
-                        '비밀번호 : thejoin10',
-                        style: TextStyle(
-                          fontSize: 20.0,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              SmoothPageIndicator(
-                controller: controller, // PageController
-                count: 2,
-                effect: SlideEffect(), // your preferred effect
-              ),
-            ],
+          WifiCard(
+            floor: '2층',
+            wifiName: 'ThejoinLAB_WiFI',
+            wifiPw: 'thejoin10',
+            url: 'images/wifi2f.png',
+            pagesCount: 2,
+            indicator: 1,
           ),
         ],
-      ),
-    );
-  }
-
-  SizedBox imgbox(String url) {
-    return SizedBox(
-      width: 200.0,
-      height: 200.0,
-      child: Container(
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            border: Border.all(color: Colors.blue.shade900)),
-        padding: const EdgeInsets.all(15.0),
-        child: Container(
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage(url), fit: BoxFit.fitWidth)),
-        ),
       ),
     );
   }
---
+++ reservation_test/lib/pages/wifi/wifiCard.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+class WifiCard extends StatelessWidget {
+  const WifiCard(
+      {Key? key,
+      required this.floor,
+      required this.wifiName,
+      required this.wifiPw,
+      required this.url,
+      required this.pagesCount,
+      required this.indicator})
+      : super(key: key);
+  final String floor;
+  final String wifiName;
+  final String wifiPw;
+  final String url;
+  final int pagesCount;
+  final int indicator;
+
+  // int contorller (int pages) {
+  //   return int pages;
+  // }
+
+  SizedBox imgbox(String url) {
+    return SizedBox(
+      width: 200.0,
+      height: 200.0,
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            border: Border.all(color: Colors.blue.shade900)),
+        padding: const EdgeInsets.all(15.0),
+        child: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage(url), fit: BoxFit.fitWidth)),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    PageController controller =
+        PageController(initialPage: 0, viewportFraction: 1);
+
+    return Column(
+      children: [
+        SizedBox(
+          width: 400.0,
+          height: 450.0,
+          child: Card(
+            margin: EdgeInsets.all(30.0),
+            shape: RoundedRectangleBorder(
+              side: BorderSide(color: Colors.blue.shade900),
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 0.0, bottom: 10.0),
+                ),
+                Text(
+                  floor,
+                  style: TextStyle(
+                    fontSize: 35.0,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                imgbox(url),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  wifiName,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                  ),
+                ),
+                Text(
+                  wifiPw,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        SmoothPageIndicator(
+          controller: PageController(
+              initialPage: indicator, viewportFraction: 1), // PageController
+          count: pagesCount,
+          effect: SlideEffect(), // your preferred effect
+        ),
+      ],
+    );
+  }
+}
