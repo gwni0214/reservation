625333d0b4ebf5d6a205c40342cb26a46098f77c
kj-thejoin <108256829+kj-thejoin@users.noreply.github.com>
2022-08-29 14:04:24 +0900

    reservWrite

--- reservation_test/lib/pages/reserve/reserve_write.dart
+++ reservation_test/lib/pages/reserve/reserve_write.dart
@@ -7,172 +7,164 @@ class ReserveWrite extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('예약하기'),
-        backgroundColor: Color.fromARGB(
-          255,
-          26,
-          35,
-          126,
+    return SafeArea(
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          title: Text('예약하기'),
+          backgroundColor: Color.fromARGB(
+            255,
+            26,
+            35,
+            126,
+          ),
+          centerTitle: true,
         ),
-        centerTitle: true,
-      ),
-      body: Container(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          children: [
-            Text(
-              '3층 회의실',
-              style: TextStyle(
-                fontSize: 30.0,
-              ),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Container(
-              width: 500.0,
-              height: 40.0,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text('09:30 ~ 10:30'),
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.blue.shade900,
-                  textStyle: TextStyle(fontSize: 25.0),
-                ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 10.0),
-              width: 500.0,
-              height: 40.0,
-              child: Center(
-                child: Text(
-                  '참여자',
-                  style: TextStyle(
-                    fontSize: 25.0,
-                    color: Colors.white,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              color: Colors.blue.shade900,
-            ),
-            Container(
-              width: 500.0,
-              height: 70.0,
-              padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
-              decoration: BoxDecoration(
-                // borderRadius: BorderRadius.circular(10.0),
-                color: Colors.white,
-                border: Border.all(
-                  color: Colors.blue.shade900,
-                  width: 3,
-                ),
-              ),
-              child: GestureDetector(
-                onTap: () => FocusScope.of(context).unfocus(),
-                child: SingleChildScrollView(
-                  child: TextField(
-                    onChanged: (text) {},
-                    cursorColor: Colors.blue.shade900,
-                    decoration: InputDecoration(
-                      focusedBorder: InputBorder.none,
-                      border: InputBorder.none,
-                      hintText: '이름을 입력하세요',
-                      hintStyle: TextStyle(fontSize: 15.0),
-                    ),
-                    keyboardType: TextInputType.text,
-                  ),
-                ),
-              ),
-              // child: Text(
-              //   overflow: TextOverflow.fade,
-              //   '12345',
-              //   style: TextStyle(
-              //     height: 1,
-              //     color: Colors.black,
-              //     fontSize: 16.0,
-              //   ),
-              // ),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 10.0),
-              width: 500.0,
-              height: 40.0,
-              child: Center(
-                child: Text(
-                  '회의 주제',
-                  style: TextStyle(
-                    fontSize: 25.0,
-                    color: Colors.white,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              color: Colors.blue.shade900,
-            ),
-            Container(
-              width: 500.0,
-              height: 70.0,
-              padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
-              decoration: BoxDecoration(
-                // borderRadius: BorderRadius.circular(10.0),
-                color: Colors.white,
-                border: Border.all(
-                  color: Colors.blue.shade900,
-                  width: 3,
-                ),
-              ),
-              child: Text(
-                overflow: TextOverflow.fade,
-                '12345',
+        body: Container(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              Text(
+                '3층 회의실',
                 style: TextStyle(
-                  height: 1,
-                  color: Colors.black,
-                  fontSize: 16.0,
+                  fontSize: 30.0,
                 ),
               ),
-            ),
-            Expanded(
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: SizedBox(
-                  width: double.infinity,
-                  height: kToolbarHeight,
-                  child: ElevatedButton(
-                    child: Text(
-                      '예약',
-                      style: TextStyle(
-                        fontSize: 22.0,
+              SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10.0),
+                width: 500.0,
+                height: 40.0,
+                child: Text(
+                  '09:30 ~ 10:00',
+                  style: TextStyle(
+                    height: 1.5,
+                    fontSize: 25.0,
+                    color: Colors.white,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                color: Colors.blue.shade900,
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10.0),
+                width: 500.0,
+                height: 40.0,
+                child: Center(
+                  child: Text(
+                    '참여자',
+                    style: TextStyle(
+                      height: 1.5,
+                      fontSize: 25.0,
+                      color: Colors.white,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                color: Colors.blue.shade900,
+              ),
+              Container(
+                width: 500.0,
+                height: 70.0,
+                padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+                decoration: BoxDecoration(
+                  // borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.white,
+                  border: Border.all(
+                    color: Colors.blue.shade900,
+                    width: 3,
+                  ),
+                ),
+                child: GestureDetector(
+                  onTap: () => FocusScope.of(context).unfocus(),
+                  child: SingleChildScrollView(
+                    child: TextField(
+                      onChanged: (text) {},
+                      cursorColor: Colors.blue.shade900,
+                      decoration: InputDecoration(
+                        focusedBorder: InputBorder.none,
+                        border: InputBorder.none,
+                        hintText: '이름을 입력하세요',
+                        hintStyle: TextStyle(fontSize: 15.0),
+                      ),
+                      keyboardType: TextInputType.text,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10.0),
+                width: 500.0,
+                height: 40.0,
+                child: Center(
+                  child: Text(
+                    '회의 주제',
+                    style: TextStyle(
+                      height: 1.5,
+                      fontSize: 25.0,
+                      color: Colors.white,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                color: Colors.blue.shade900,
+              ),
+              Container(
+                width: 500.0,
+                height: 70.0,
+                padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+                decoration: BoxDecoration(
+                  // borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.white,
+                  border: Border.all(
+                    color: Colors.blue.shade900,
+                    width: 3,
+                  ),
+                ),
+                child: GestureDetector(
+                  onTap: () => FocusScope.of(context).unfocus(),
+                  child: SingleChildScrollView(
+                    child: TextField(
+                      onChanged: (text) {},
+                      cursorColor: Colors.blue.shade900,
+                      decoration: InputDecoration(
+                        focusedBorder: InputBorder.none,
+                        border: InputBorder.none,
+                        hintText: '회의 주제를 입력하세요',
+                        hintStyle: TextStyle(fontSize: 15.0),
+                      ),
+                      keyboardType: TextInputType.text,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: kToolbarHeight,
+                    child: ElevatedButton(
+                      child: Text(
+                        '예약',
+                        style: TextStyle(
+                          fontSize: 22.0,
+                        ),
+                      ),
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.blue.shade900,
                       ),
                     ),
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.blue.shade900,
-                    ),
                   ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
-
-      // bottomNavigationBar: BottomNavigationBar(
-      //   onTap: (index) {},
-      //   items: [
-      //     BottomNavigationBarItem(
-      //       icon: Icon(Icons.check_box),
-      //       label: '예약하기',
-      //     ),
-      //     BottomNavigationBarItem(
-      //       icon: Icon(Icons.check_box),
-      //       label: '2',
-      //     ),
-      //   ],
-      // ),
     );
   }
 }
