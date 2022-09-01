2ce123e2bc677623966a1cf359b887053a224eaf
kj-thejoin <108256829+kj-thejoin@users.noreply.github.com>
2022-09-01 20:01:57 +0900

    login

--- reservation_test/lib/pages/login/login.dart
+++ reservation_test/lib/pages/login/login.dart
@@ -1,6 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:reservation_test/pages/main/home.dart';
+import 'dart:developer';
+import 'package:reservation_test/models/model.dart';
 
 class Login extends StatelessWidget {
   static const id = '/login';
@@ -8,9 +12,8 @@ class Login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final TextEditingController userId = TextEditingController();
-    final TextEditingController userPw = TextEditingController();
-
+    late String userId = '';
+    late String userPassword = '';
     return Scaffold(
       // appBar: AppBar(
       //   title: Text('Login 페이지'),
@@ -68,16 +71,21 @@ class Login extends StatelessWidget {
                           SizedBox(
                             height: 30.0,
                           ),
-                          TextFieldWidget(
-                            "ID를 입력하세요",
-                            userId,
+                          InputForm(
+                            label: 'id를 입력하세요',
+                            onChange: (value) {
+                              userId = value;
+                            },
                           ),
                           SizedBox(
                             height: 15.0,
                           ),
-                          TextFieldWidget(
-                            "비밀번호를 입력하세요",
-                            userPw,
+                          InputForm(
+                            label: 'Pw를 입력하세요',
+                            onChange: (value) {
+                              userPassword = value;
+                            },
+                            isPassword: true,
                           ),
                           SizedBox(
                             height: 15.0,
@@ -90,8 +98,8 @@ class Login extends StatelessWidget {
                               height: 50.0,
                               child: ElevatedButton(
                                 onPressed: () {
-                                  if (userId.text == "thejoin" &&
-                                      userPw.text == "1234") {
+                                  if (userId == "thejoin" &&
+                                      userPassword == "1234") {
                                     Get.offAllNamed(Home.id);
                                   } else {
                                     ScaffoldMessenger.of(context).showSnackBar(
@@ -143,44 +151,79 @@ class Login extends StatelessWidget {
   }
 }
 
-Widget TextFieldWidget(inputValue, userInfo) {
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      TextField(
-        controller: userInfo,
-        onChanged: (value) {},
-        decoration: InputDecoration(
-          labelText: inputValue,
-          enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(
-              width: 3,
-              color: Color.fromARGB(
-                255,
-                26,
-                35,
-                126,
+class InputForm extends StatefulWidget {
+  InputForm(
+      {Key? key,
+      required this.label,
+      required this.onChange,
+      this.isPassword = false})
+      : super(key: key);
+
+  final String label;
+
+  final bool isPassword;
+
+  final Function(String) onChange;
+  @override
+  State<InputForm> createState() => _InputFormState();
+}
+
+class _InputFormState extends State<InputForm> {
+  final TextEditingController inputController = TextEditingController();
+  late String CheckText = '';
+
+  @override
+  void initState() {
+    super.initState();
+    inputController.text = '';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        TextField(
+          obscureText: widget.isPassword,
+          controller: inputController,
+          onChanged: (value) {
+            widget.onChange(value);
+            setState(() {
+              CheckText = value;
+            });
+          },
+          decoration: InputDecoration(
+            labelText: widget.label,
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                width: 3,
+                color: Color.fromARGB(
+                  255,
+                  26,
+                  35,
+                  126,
+                ),
+              ),
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.grey,
               ),
             ),
           ),
-          focusedBorder: UnderlineInputBorder(
-            borderSide: BorderSide(
-              color: Colors.grey,
+          keyboardType: TextInputType.text,
+        ),
+        Visibility(
+          visible: CheckText == '',
+          child: Text(
+            '글자를 입력하세요',
+            style: TextStyle(
+              fontSize: 11.0,
+              color: Colors.redAccent,
             ),
           ),
         ),
-        keyboardType: TextInputType.text,
-      ),
-      Visibility(
-        visible: userInfo.text == '',
-        child: Text(
-          '글자를 입력하세요',
-          style: TextStyle(
-            fontSize: 11.0,
-            color: Colors.redAccent,
-          ),
-        ),
-      ),
-    ],
-  );
+      ],
+    );
+  }
 }
